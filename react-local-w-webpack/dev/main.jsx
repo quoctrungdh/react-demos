5b7aa9e3b82e0f40e4ef7755cd99c85d67eb780c
quoc-trung.do-ha <quoc-trung.do-ha@fullsix.com>
2016-09-23 18:36:24 +0700

    adding examples

--- react-local-w-webpack/dev/main.jsx
+++ react-local-w-webpack/dev/main.jsx
@@ -1,33 +1,145 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-var HelloWorld = React.createClass({
+// var HelloWorld = React.createClass({
+//   render: function() {
+//     return(
+//       <p>Hello {this.props.greetTarget} </p>
+//     );
+//   }
+// });
+//
+// var RenderBlock = React.createClass({
+//   render: function() {
+//     var data = this.props.data;
+//     var renderData = [];
+//     for(var i=0; i<data.length; i++) {
+//       renderData.push(<HelloWorld key={data[i]} greetTarget={data[i]} /> );
+//     }
+//     return (
+//       <div>
+//         {renderData}
+//       </div>
+//     );
+//   }
+// });
+//
+// var dest = document.getElementById('content');
+// var heroes = ["Batman", "Superman", "Spiderman"];
+//
+// ReactDOM.render(
+//   <RenderBlock data={heroes}/>,
+//   dest
+// );
+
+// var Card = React.createClass({
+//   render: function() {
+//     var cardStyle = {
+//       height: 200,
+//       width: 150,
+//       display: "inline-block",
+//       margin: 30,
+//       backgroundColor: "#fff",
+//       filter: "drop-shadow(0px 0px 5px #666)"
+//     };
+//     return(
+//       <div style={cardStyle}>
+//         <Square color={this.props.color}/>
+//         <Label color={this.props.color}/>
+//       </div>
+//     );
+//   }
+// });
+//
+// var Square = React.createClass({
+//   render: function() {
+//     var squareStyle = {
+//       height: 150,
+//       backgroundColor: this.props.color
+//     }
+//     return(
+//       <div style={squareStyle}>
+//       </div>
+//     );
+//   }
+// });
+//
+// var Label = React.createClass({
+//   render: function() {
+//     var labelStyle = {
+//       fontFamily: "sans-serif",
+//       fontWeight: "bold",
+//       textAlign: "center",
+//       padding: 16,
+//       margin: 0
+//     }
+//     return(
+//       <p style={labelStyle}>{this.props.color}</p>
+//     );
+//   }
+// });
+//
+// var dest = document.getElementById('content');
+// ReactDOM.render(
+//   <div>
+//     <Card color="#FF6663"/>
+//     <Card color="#FFA737"/>
+//     <Card color="#A763FF"/>
+//     <Card color="#37F6F6"/>
+//     <Card color="#FFA737"/>
+//     <Card color="#37F6F6"/>
+//     <Card color="#FF6663"/>
+//     <Card color="#A763FF"/>
+//   </div>,
+//   dest
+// );
+
+var strikes = 0;
+
+var LightningCounter = React.createClass({
+  getInitialState: function() {
+    return {
+      strikes: 0
+    }
+  },
+  timerTick: function() {
+    this.setState({
+      strikes: this.state.strikes + 100
+    });
+  },
+  componentDidMount: function() {
+    setInterval(this.timerTick, 1000);
+  },
   render: function() {
-    return(
-      <p>Hello {this.props.greetTarget} </p>
+    return (
+        <h1>{this.state.strikes}</h1>
     );
   }
 });
 
-var RenderBlock = React.createClass({
+
+var LightningCounterDisplay = React.createClass({
   render: function() {
-    var data = this.props.data;
-    var renderData = [];
-    for(var i=0; i<data.length; i++) {
-      renderData.push(<HelloWorld key={data[i]} greetTarget={data[i]} /> );
-    }
+    var counterStyle = {
+      width: 250,
+      height: 100,
+      padding: 40,
+      backgroundColor: "#000",
+      color: "#fff",
+      fontFamily: "sans-serif",
+      borderRadius: 10
+    };
     return (
-      <div>
-        {renderData}
+      <div style={counterStyle}>
+        <LightningCounter/>
       </div>
     );
   }
 });
 
-var dest = document.getElementById('content');
-var heroes = ["Batman", "Superman", "Spiderman"];
 
+var dest = document.getElementById('content');
 ReactDOM.render(
-  <RenderBlock data={heroes}/>,
+  <LightningCounterDisplay />,
   dest
 );
--- react-local-w-webpack/output/main.js
+++ react-local-w-webpack/output/main.js
@@ -56,41 +56,149 @@
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-	var HelloWorld = _react2.default.createClass({
-	  displayName: "HelloWorld",
+	// var HelloWorld = React.createClass({
+	//   render: function() {
+	//     return(
+	//       <p>Hello {this.props.greetTarget} </p>
+	//     );
+	//   }
+	// });
+	//
+	// var RenderBlock = React.createClass({
+	//   render: function() {
+	//     var data = this.props.data;
+	//     var renderData = [];
+	//     for(var i=0; i<data.length; i++) {
+	//       renderData.push(<HelloWorld key={data[i]} greetTarget={data[i]} /> );
+	//     }
+	//     return (
+	//       <div>
+	//         {renderData}
+	//       </div>
+	//     );
+	//   }
+	// });
+	//
+	// var dest = document.getElementById('content');
+	// var heroes = ["Batman", "Superman", "Spiderman"];
+	//
+	// ReactDOM.render(
+	//   <RenderBlock data={heroes}/>,
+	//   dest
+	// );
 
+	// var Card = React.createClass({
+	//   render: function() {
+	//     var cardStyle = {
+	//       height: 200,
+	//       width: 150,
+	//       display: "inline-block",
+	//       margin: 30,
+	//       backgroundColor: "#fff",
+	//       filter: "drop-shadow(0px 0px 5px #666)"
+	//     };
+	//     return(
+	//       <div style={cardStyle}>
+	//         <Square color={this.props.color}/>
+	//         <Label color={this.props.color}/>
+	//       </div>
+	//     );
+	//   }
+	// });
+	//
+	// var Square = React.createClass({
+	//   render: function() {
+	//     var squareStyle = {
+	//       height: 150,
+	//       backgroundColor: this.props.color
+	//     }
+	//     return(
+	//       <div style={squareStyle}>
+	//       </div>
+	//     );
+	//   }
+	// });
+	//
+	// var Label = React.createClass({
+	//   render: function() {
+	//     var labelStyle = {
+	//       fontFamily: "sans-serif",
+	//       fontWeight: "bold",
+	//       textAlign: "center",
+	//       padding: 16,
+	//       margin: 0
+	//     }
+	//     return(
+	//       <p style={labelStyle}>{this.props.color}</p>
+	//     );
+	//   }
+	// });
+	//
+	// var dest = document.getElementById('content');
+	// ReactDOM.render(
+	//   <div>
+	//     <Card color="#FF6663"/>
+	//     <Card color="#FFA737"/>
+	//     <Card color="#A763FF"/>
+	//     <Card color="#37F6F6"/>
+	//     <Card color="#FFA737"/>
+	//     <Card color="#37F6F6"/>
+	//     <Card color="#FF6663"/>
+	//     <Card color="#A763FF"/>
+	//   </div>,
+	//   dest
+	// );
+
+	var strikes = 0;
+
+	var LightningCounter = _react2.default.createClass({
+	  displayName: "LightningCounter",
+
+	  getInitialState: function getInitialState() {
+	    return {
+	      strikes: 0
+	    };
+	  },
+	  timerTick: function timerTick() {
+	    this.setState({
+	      strikes: this.state.strikes + 100
+	    });
+	  },
+	  componentDidMount: function componentDidMount() {
+	    setInterval(this.timerTick, 1000);
+	  },
 	  render: function render() {
 	    return _react2.default.createElement(
-	      "p",
+	      "h1",
 	      null,
-	      "Hello ",
-	      this.props.greetTarget,
-	      " "
+	      this.state.strikes
 	    );
 	  }
 	});
 
-	var RenderBlock = _react2.default.createClass({
-	  displayName: "RenderBlock",
+	var LightningCounterDisplay = _react2.default.createClass({
+	  displayName: "LightningCounterDisplay",
 
 	  render: function render() {
-	    var data = this.props.data;
-	    var renderData = [];
-	    for (var i = 0; i < data.length; i++) {
-	      renderData.push(_react2.default.createElement(HelloWorld, { key: data[i], greetTarget: data[i] }));
-	    }
+	    var counterStyle = {
+	      width: 250,
+	      height: 100,
+	      padding: 40,
+	      backgroundColor: "#000",
+	      color: "#fff",
+	      fontFamily: "sans-serif",
+	      borderRadius: 10
+	    };
 	    return _react2.default.createElement(
 	      "div",
-	      null,
-	      renderData
+	      { style: counterStyle },
+	      _react2.default.createElement(LightningCounter, null)
 	    );
 	  }
 	});
 
 	var dest = document.getElementById('content');
-	var heroes = ["Batman", "Superman", "Spiderman"];
-
-	_reactDom2.default.render(_react2.default.createElement(RenderBlock, { data: heroes }), dest);
+	_reactDom2.default.render(_react2.default.createElement(LightningCounterDisplay, null), dest);
 
 /***/ },
 /* 1 */
